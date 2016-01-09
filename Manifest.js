8b5e5775dd4484e52d5b30d4ab3691417e2a57eb
edoardoo <edoubuntu@gmail.com>
2016-01-09 02:00:37 +0000

    First commit.

---
+++ Manifest.js
@@ -0,0 +1,13 @@
+{
+  "manifest_version": 2,
+  "name": "keepTube",
+  "description": "Keep youtube playing, always.",
+  "version": "1.0",
+  "browser_action": {
+  },
+  "permissions": [
+    "activeTab",
+    "https://ajax.googleapis.com/",
+    "tabs"
+  ]
+}
