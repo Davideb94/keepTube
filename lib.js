1295fd93ef77d388729854fc65bacd5e2da21030
edoardoo <edoubuntu@gmail.com>
2016-01-10 01:06:12 +0000

    Added graphics and logo.

--- lib.js
+++
@@ -1,8 +0,0 @@
-$(".ytp-right-controls").append("<button class='ytp-keep-button ytp-button'>p&lrtri;</button>");
-$(".ytp-keep-button").on('click', function(){
-	console.log( document.URL.split("&")[0] );
-	if($("#movie_player").hasClass("playing-mode")){
-		$(".ytp-play-button").click();
-	}
-	chrome.extension.sendRequest({ msg: document.URL.split("&")[0].split("?")[1].split("=")[1] });
-});
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("Yahoooooooga");
 function loadPanel( videoUrl ){
 	console.dir(videoUrl);
-	chrome.windows.create({ url: 'player.html?url='+videoUrl, type: 'panel' });
+	chrome.windows.create({ url: 'panel/player.html?url='+videoUrl, type: 'panel' });
 	//chrome.windows.create({ url: 'player.html?url='+videoUrl});
 }	
 chrome.extension.onRequest.addListener(
