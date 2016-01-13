ea07c1ebf84f0896789124933af04bdffb9c4020
edoardoo <edoubuntu@gmail.com>
2016-01-13 00:56:15 +0000

    Fixed player width. Switched to scss.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.log("Yahoooooooga");
 function loadPanel( videoUrl ){
 	console.dir(videoUrl);
-	//chrome.windows.create({ url: 'panel/player.html?url='+videoUrl, type: 'panel' });
-	chrome.windows.create({ url: 'http://www.youtube.com/watch?v='+videoUrl+'&strip=true', type: 'panel'});
+	// chrome.windows.create({ url: 'http://www.youtube.com/watch?v='+videoUrl+'&strip=true', type: 'panel'});
+	chrome.windows.create({ url: 'http://www.youtube.com/watch?v='+videoUrl+'&strip=true'});
 }	
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse){
