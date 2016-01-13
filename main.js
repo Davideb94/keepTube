1a14c2f45a8b441ada10ec8cfe98de888aa38408
edoardoo <edoubuntu@gmail.com>
2016-01-13 19:18:55 +0000

    Fixed player with wrong dimensions on panel: now works on every dimension. Fixed favicon with base64 converted image.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-console.log("Yahoooooooga");
 function loadPanel( videoUrl ){
-	console.dir(videoUrl);
+	console.log(videoUrl);
 	// chrome.windows.create({ url: 'http://www.youtube.com/watch?v='+videoUrl+'&strip=true', type: 'panel'});
-	chrome.windows.create({ url: 'http://www.youtube.com/watch?v='+videoUrl+'&strip=true'});
+	chrome.windows.create({ 
+		url: 'http://www.youtube.com/watch?v='+videoUrl+'&strip=true', 
+		height: 230, 
+		width: 330,
+		type: "panel"
+	});
 }	
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse){
-        loadPanel(request.msg);
+        loadPanel(request.url);
     }
 );
